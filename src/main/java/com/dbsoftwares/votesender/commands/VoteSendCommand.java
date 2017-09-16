79203f35ae772101d98e98bcd469ec94bf140278
didjee2 <dieter08@live.be>
2017-09-16 13:08:30 +0200

    Updated messages not being colored

--- src/main/java/com/dbsoftwares/votesender/commands/VoteSendCommand.java
+++ src/main/java/com/dbsoftwares/votesender/commands/VoteSendCommand.java
@@ -8,6 +8,7 @@ package com.dbsoftwares.votesender.commands;
 
 import com.dbsoftwares.votesender.VoteSender;
 import com.dbsoftwares.votesender.processor.VoteProcessor;
+import com.dbsoftwares.votesender.utils.Utilities;
 import com.google.common.collect.Lists;
 import com.vexsoftware.votifier.model.Vote;
 import org.bukkit.command.Command;
@@ -25,13 +26,13 @@ public class VoteSendCommand extends Command {
     @Override
     public boolean execute(CommandSender sender, String aliase, String[] args) {
         if (!sender.hasPermission("votesend.admin")) {
-            sender.sendMessage("&c&lVoteSender &fYou don't have the permission to do this!");
+            sender.sendMessage(Utilities.format("&c&lVoteSender &fYou don't have the permission to do this!"));
             return false;
         }
         if (args.length == 1) {
             if (args[0].contains("reload") || args[0].contains("rl")) {
                 VoteSender.getInstance().reload();
-                sender.sendMessage("&c&lVoteSender &fThe config has been reloaded!");
+                sender.sendMessage(Utilities.format("&c&lVoteSender &fThe config has been reloaded!"));
                 return true;
             }
         } else if(args.length == 3 && args[0].contains("send")) {
@@ -45,14 +46,14 @@ public class VoteSendCommand extends Command {
             vote.setTimeStamp(String.valueOf(new Date().getTime()));
             new VoteProcessor(vote);
 
-            sender.sendMessage("&c&lVoteSender &fYou have sent a test vote for &c" + player + "&f!");
+            sender.sendMessage(Utilities.format("&c&lVoteSender &fYou have sent a test vote for &c" + player + "&f!"));
             return true;
         }
-        sender.sendMessage("&c&lVoteSender &fHelp:");
-        sender.sendMessage("&c- &f/votesend reload &7| Reloads the plugin.");
-        sender.sendMessage("&c- &f/votesend send (player) (service) &7| Sends a test vote.");
-        sender.sendMessage("&c- &f/votesend help &7| Returns this list.");
-        sender.sendMessage("&c- &f/votesend amount &7| Returns the amount of votes sent through.");
+        sender.sendMessage(Utilities.format("&c&lVoteSender &fHelp:"));
+        sender.sendMessage(Utilities.format("&c- &f/votesend reload &7| Reloads the plugin."));
+        sender.sendMessage(Utilities.format("&c- &f/votesend send (player) (service) &7| Sends a test vote."));
+        sender.sendMessage(Utilities.format("&c- &f/votesend help &7| Returns this list."));
+        sender.sendMessage(Utilities.format("&c- &f/votesend amount &7| Returns the amount of votes sent through."));
         return false;
     }
 }
